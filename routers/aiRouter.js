92762305f267b3a2601b2f2901df1e8cda372d30
구동희 <71062817+gudonghee2000@users.noreply.github.com>
2022-03-17 18:33:29 +0900

    Delete routers directory

--- routers/aiRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getAiPage, postAiPage } from "../controller/aiController";
-
-const aiRouter = express.Router();
-
-aiRouter.route("").get(getAiPage).post(postAiPage);
-
-export default aiRouter;
